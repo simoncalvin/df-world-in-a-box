e1061d2c93bcc011e8bc61e69f732a1bb9805eb9
Ubuntu <simon@dfdocker.ulrxjedzjreefnqjzobefppiha.bx.internal.cloudapp.net>
2018-08-06 03:58:14 +0000

    fixes output path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,18 +13,18 @@ echo "Creating output directory"
 world=$(head -n 1 ./df_linux/region1-00030-01-01-world_history.txt)
 world=${world// /_}
 
-echo $world
+echo "Generated: $world"
 
 mkdir -p /world_gen/$(hostname)/$world
 
-echo "Copying output"
+echo "Copying output to {volume}/$(hostname)"
 
 for path in ./df_linux/region1*; do
 	file=$(basename $path)
-	cp $path /world_gen/$(hostname)/${file/region1-00030-01-01/$world};
+	cp $path /world_gen/$(hostname)/${file/region1/$world};
 done
 
-cp -r  ./df_linux/data/save/region1/ /world_gen/$(hostname)/$world
+cp -frp  ./df_linux/data/save/region1 -T /world_gen/$(hostname)/$world
 
 echo "Exited $0"
 
